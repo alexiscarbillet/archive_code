bfab4310084873921558566bd3e6bb93ccb705d1
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2025-03-28 08:18:23 -0400

    add more react examples

---
+++ react_examples/passing_props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from './Heading';
+
+function App() {
+    return (
+        <div>
+            <Heading firstName="Bob" />
+            <Heading firstName="Alice" />
+        </div>
+    );
+}
+
+export default App;
